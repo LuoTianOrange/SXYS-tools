9e2643dec43228383483966bb92f11c1d0fefade
RaitoOrange <2977277015@qq.com>
2024-02-07 05:02:51 +0800

    1.0版本

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import MainPage from '@/views/MainPage.vue'
+console.log("Powered by Vue 3 + vite + elemtment-plus");
+console.log("作者:灯火橘，数据来自Bilibili@风云de寒冰");
+</script>
+
+<template>
+  <MainPage></MainPage>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
