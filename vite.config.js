3bef813c2031e3370f32373c3f0fc4e81302b1e0
RaitoOrange <2977277015@qq.com>
2024-02-07 05:28:27 +0800

    提交

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['@element-plus/icons-vue']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@element-plus/icons-vue']
+  //   }
+  // }
 })
